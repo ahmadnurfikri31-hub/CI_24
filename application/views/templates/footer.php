fb08dbba5093e3a4866aa8737903e93f03ff05ea
ahmadnurfikri31-hub <ahmadnurfikri31@gmail.com>
2026-04-21 15:41:06 +0700

    pertemuan_8

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,12 +1,12 @@
 </div>
 </div>
 
-
 <script src="<?= base_url('assets/vendor/jquery/jquery.min.js'); ?>"></script>
 <script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js');?>"></script>
 <!-- DataTables CSS -->
 <script src="<?= base_url('assets/vendor/datatables/jquery.dataTables.min.js') ?>"></script>
 <script src="<?= base_url('assets/vendor/datatables/dataTables.bootstrap4.min.js') ?>"></script> 
+<script src="<?= base_url('assets/vendor/chart.js/Chart.min.js');?>"></script>
 <script src="<?= base_url('assets/js/sb-admin-2.min.js');?>"></script>
 
 <script>
@@ -24,5 +24,35 @@
     });
 });
 </script>
+<script>
+    var ctx= document.getElementById("chartDashboard");
+    var chart= new Chart(ctx,{
+        type:'bar',
+        data:{
+            labels:['kategori', 'anggota'],
+            datasets:[{
+            labels:'jumlah data',
+            data:[
+                <?= $total_kategori; ?>,
+                <?= $total_anggota; ?>
+            ],
+            backgroundColor:[
+                '#4e73df',
+                '#1cc88a'
+            ]
+        }]
+    },
+    options:{
+         responsive: true,
+         scales:{
+              yAxes:[{
+                ticks:{
+                    beginAtZero: true
+                   }
+             }]
+           }
+        }
+    });
+    </script>
 </body>
 </html>
